9553dd02d3a9acf3b65391e088b1e2f58356a599
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-06-29 14:07:15 +0300

    Добавил 3 задание из темы 1

---
+++ WpfHomeWorksTask1_3/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHomeWorksTask1_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(0, (int)MainGrid.ActualWidth - (int)Button.Width);
+            int y = rnd.Next(0, (int)MainGrid.ActualHeight - (int)Button.Height);
+            Button.Margin = new Thickness(x, y, 0, 0);
+        }
+    }
+}
