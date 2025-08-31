3751990aa388cb3f0214ca82c45d2b9c6f5706ce
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-08-31 19:08:01 +0300

    Задание по 8 теме

---
+++ WpfHomeWorksTask8/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfHomeWorksTask8
+{
+    public enum TipTovara
+    {
+        Еда,
+        Бытовая_техника
+    }
+    public class Products
+    {
+        public string imya {  get; set; }
+        public int tsena {  get; set; }
+        public string? izobradzenie {  get; set; }
+        public TipTovara tipTovara {  get; set; }
+    }
+}
