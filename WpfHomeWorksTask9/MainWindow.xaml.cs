a0bf19a3fad5d7fabc2af4cdf9268df0e36815bd
Сергей Кузикян <ferrari2008gt@mail.ru>
2025-09-20 13:33:17 +0300

    Добавил решение по теме 9

---
+++ WpfHomeWorksTask9/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHomeWorksTask9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CurrentColor = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+        }
+        
+        // Коллекция для хранения последовательности цветов
+        private Stack<Brush> _colorHistory = new Stack<Brush>();
+
+        private Brush _currentColor;
+
+        // Текущий цвет
+        public Brush CurrentColor
+        {
+            get => _currentColor;
+            set
+            {
+                _currentColor = value;
+
+                // Добавляем новый цвет в стек, если предыдущий цвет не такой же
+                // (либо стек пустой) 
+                if (_colorHistory.Count == 0 || _colorHistory.Peek() != value)
+                {
+                    _colorHistory.Push(value);
+                }
+                // Grid - это имя (x:Name) контейнера компоновки, для которого меняем цвет
+                Grid.Background = _currentColor;
+            }
+        }
+        
+        private void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var random = new Random();
+            // CurrentColor - это свойство для хранения текущего цвета
+            // ниже описано, как оно устроено
+            CurrentColor = new SolidColorBrush(Color.FromRgb(
+                (byte)random.Next(256),
+                (byte)random.Next(256),
+                (byte)random.Next(256)));
+        }
+        private void UndoExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_colorHistory.Count > 1)
+            {
+                _colorHistory.Pop(); // Удаляем текущий цвет
+                CurrentColor = _colorHistory.Pop(); // Берём предыдущий
+            }
+        }
+
+        private void UndoCanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {            
+                if (_colorHistory != null && _colorHistory.Count > 1)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+    }
+}
